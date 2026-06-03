73bce13daf45e68a3c8d2b5da84d1cdf24d92c37
MD ABDUR RAHMAN SHOAIB <shoaibontheway@gmail.com>
2026-06-03 22:40:32 +0600

    Display created and active jobs

--- src/app/dashboard/recruiter/jobs/page.jsx
+++ src/app/dashboard/recruiter/jobs/page.jsx
@@ -1,6 +1,10 @@
+import { getCompanyJobs } from "@/lib/api/jobs";
 import React from "react";
 
-const RecruiterJobsPage = () => {
+const RecruiterJobsPage = async () => {
+  const companyId = 'company_123'; //todo: 
+  const jobs = await getCompanyJobs(companyId);
+  console.log(jobs, 'jobs from recruiter page');
   return (
     <div>
       <h1>Recruiter/company&apos;s manage All Jobs!</h1>
