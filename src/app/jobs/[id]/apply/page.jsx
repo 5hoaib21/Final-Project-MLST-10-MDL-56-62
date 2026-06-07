3e3374067500a6e848a47d8acba7f0a67ae44f1e
MD ABDUR RAHMAN SHOAIB <shoaibontheway@gmail.com>
2026-06-08 03:00:37 +0600

    Allow only job seekers to apply for jobs

--- src/app/jobs/[id]/apply/page.jsx
+++ src/app/jobs/[id]/apply/page.jsx
@@ -1,8 +1,10 @@
+import { gerJobById } from "@/lib/api/jobs";
 import { getUserSession } from "@/lib/core/session";
 import { ShieldExclamation } from "@gravity-ui/icons";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import React from "react";
+import JobApply from "./JobApply";
 
 const ApplyPage = async ({ params }) => {
     const {id} = await params;
@@ -14,29 +16,18 @@ const ApplyPage = async ({ params }) => {
       // Auth Role Guard Screen
     if (user.role !== 'seeker') {
         return (
-            <div className="w-full min-h-screen bg-zinc-950 flex j">
-                <div className="max-w-md w-full text-center p-8 rounded-2xl bg-zinc-900 border border-zinc-800 shadow-xl">
-                    <div className="w-12 h-12 bg-amber-500/10 text-amber-500 rounded-full flex items-center justify-center mx-auto mb-4">
-                        <ShieldExclamation className="w-6 h-6" />
-                    </div>
-                    <h3 className="text-xl font-bold text-zinc-100 mb-2">Access Restricted</h3>
-                    <p className="text-zinc-400 text-sm leading-relaxed mb-6">
-                        Only job seekers can apply for positions. Please sign in with a seeker account to proceed.
-                    </p>
-                    <Link 
-                        href="/auth/signin" 
-                        className="inline-block w-full px-4 py-2.5 bg-zinc-800 hover:bg-zinc-700 text-zinc-200 rounded-lg text-sm font-medium transition"
-                    >
-                        Switch Account
-                    </Link>
-                </div>
+            <div className="w-full min-h-screen bg-zinc-950 flex flex-col justify-center items-center text-white p-6">
+              <p>Only job seeker can apply for positions. Please sign in with a seeker account to proceed.</p>
             </div>
         );
     }
 
+    const job = await gerJobById(id)
+
   return (
     <div className="mt-30">
-      <h2>apply for this job</h2>
+      <h2>apply for {job.jobTitle}</h2>
+      <JobApply job={job} />
     </div>
   );
 };
